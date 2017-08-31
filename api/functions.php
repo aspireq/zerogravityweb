44fe2c3c3eb918e9fd9ac8a2b9f5cd8b75336928
aspireq <mehul@aspireq.com>
2017-08-31 10:39:15 +0530

    Merge origin/master
    Conflicts:
    	application/views/visitor/homepage.php

--- api/functions.php
+++ api/functions.php
@@ -1,18 +1,18 @@
 <?php
 
-class ResellerClubAPI {   
+class ResellerClubAPI {
+
     public $api_user_id = "706625";
     public $api_key = "Y7xW7L2HPkjqTiJ59FRGCZu5dh3U0o78";
-    
     //public $api_user_id = "255388";
     //public $api_user_id = "706625";
     //public $api_key = "0363O71bJmUpwJ2W5r2M9At0Jsm0UcFA";
     //public $api_key = "Ie61qyIhGIQR45yXT2VD78rwLeOHxdNn";
-    // List of TDL's - TLDs for which the domain name availability needs to be checked
-    
-    public $tlds_list = array("com", "net", "in", "biz", "org", "asia", "co");    
+    // List of TDL's - TLDs for which the domain name availability needs to be checked    
+
+    public $tlds_list = array("com", "net", "in", "biz", "org", "asia", "co");
     public $domainname;
-    
+
     public function __construct() {
         if (isset($_POST['domain'])) {
             $this->domainname = preg_replace('/[\s-]+/', '-', substr(trim($_POST['domain']), 0, 253));
@@ -23,17 +23,17 @@ class ResellerClubAPI {
 
 //Get Domain Availability ResellerClub API
     public function DomainAvailability($y) {
-        
+
         $tld = "";
         foreach ($this->tlds_list as $arrayitem) {
             $tld .= '&tlds=' . $arrayitem;
         }
         $url = 'https://test.httpapi.com/api/domains/available.json?auth-userid=' . $this->api_user_id . '&api-key=' . $this->api_key . '&domain-name=' . $this->domainname . $tld . '&suggest-alternative=true';
         $url = 'https://test.httpapi.com/api/domains/available.json?auth-userid=' . $this->api_user_id . '&api-key=' . $this->api_key . '&domain-name=' . $this->domainname . $tld . '&suggest-alternative=true';
-        
+
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt ($curl, CURLOPT_CAINFO,getcwd().'/cacert.pem');
+        curl_setopt($curl, CURLOPT_CAINFO, getcwd() . '/cacert.pem');
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         $apidata = curl_exec($curl);
         $data = json_decode($apidata, TRUE);
